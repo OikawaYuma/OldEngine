2d1af64e59f0b8eb32a6803dc34cb80a0cdf1984
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-10-10 14:47:55 +0900

    完成

--- Src/Sprite.cpp
+++ Src/Sprite.cpp
@@ -11,124 +11,12 @@ Sprite::Sprite() {};
 void Sprite::Initialize() {
 	sWinApp = WinApp::GetInstance();
 	sDirectXCommon = DirectXCommon::GetInstance();
-
-	descriptionRootSignature.Flags =
-		D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
 	
-	// シリアライズしてバイナリにする
-	signatureBlob = nullptr;
+	rootParamerters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE; // DescripterTableを使う
+	rootParamerters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL; // PixelShaderで使う
+	rootParamerters[0].DescriptorTable.pDescriptorRanges = descriptorRange_; // Tableの中身の配列を指定
+	rootParamerters[0].DescriptorTable.NumDescriptorRanges = _countof(descriptorRange_); // Tableで利用する数
 
-	// RootParmeter作成。複数でっていできるので配列。今回は結果１つだけなので長さ1の配列
-	rootParamerters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;  // CBVを使う
-	rootParamerters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;  // PixelShaderで使う
-	rootParamerters[0].Descriptor.ShaderRegister = 0; //レジスタ番号0とバインド
-
-	rootParamerters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;  // CBVを使う
-	rootParamerters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;  // vertexShaderで使う
-	rootParamerters[1].Descriptor.ShaderRegister = 0; //レジスタ番号0とバインド
-
-	descriptorRange_[0].BaseShaderRegister = 0; // 0から始まる
-	descriptorRange_[0].NumDescriptors = 1; // 数は1つ
-	descriptorRange_[0].RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV; // SRVを使う
-	descriptorRange_[0].OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND; // Offsetを自動計算
-
-	rootParamerters[2].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE; // DescripterTableを使う
-	rootParamerters[2].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL; // PixelShaderで使う
-	rootParamerters[2].DescriptorTable.pDescriptorRanges = descriptorRange_; // Tableの中身の配列を指定
-	rootParamerters[2].DescriptorTable.NumDescriptorRanges = _countof(descriptorRange_); // Tableで利用する数
-
-	staticSamplers[0].Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR; // バイナリフィルタ
-	staticSamplers[0].AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP; // 0~1の範囲外をリピート
-	staticSamplers[0].AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
-	staticSamplers[0].AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
-	staticSamplers[0].ComparisonFunc = D3D12_COMPARISON_FUNC_NEVER; // 比較しない
-	staticSamplers[0].MaxLOD = D3D12_FLOAT32_MAX; // ありったけのMipmapを使う
-	staticSamplers[0].ShaderRegister = 0; // レジスタ番号0を使う
-	staticSamplers[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL; // PixelShaderで使う
-
-	descriptionRootSignature.pParameters = rootParamerters; // ルートパラメータ配列へのポインタ
-	descriptionRootSignature.NumParameters = _countof(rootParamerters); // 配列の長さ
-	descriptionRootSignature.pStaticSamplers = staticSamplers;
-	descriptionRootSignature.NumStaticSamplers = _countof(staticSamplers);
-
-	errorBlob = nullptr;
-	hr_ = D3D12SerializeRootSignature(&descriptionRootSignature,
-		D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob, &errorBlob);
-	if (FAILED(hr_)) {
-		ConsoleLog(reinterpret_cast<char*>(errorBlob->GetBufferPointer()));
-		assert(false);
-	}
-	// バイナリを元に生成
-	rootSignature = nullptr;
-	hr_ = sDirectXCommon->GetDevice()->CreateRootSignature(0, signatureBlob->GetBufferPointer(),
-		signatureBlob->GetBufferSize(), IID_PPV_ARGS(&rootSignature));
-	assert(SUCCEEDED(hr_));
-
-	// InputLayout
-	inputElementDescs[0].SemanticName = "POSITION";
-	inputElementDescs[0].SemanticIndex = 0;
-	inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
-	inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
-	inputElementDescs[1].SemanticName = "TEXCOORD";
-	inputElementDescs[1].SemanticIndex = 0;
-	inputElementDescs[1].Format = DXGI_FORMAT_R32G32_FLOAT;
-	inputElementDescs[1].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
-
-	inputLayoutDesc.pInputElementDescs = inputElementDescs;
-	inputLayoutDesc.NumElements = _countof(inputElementDescs);
-
-	// blendStateの設定
-	//すべての色要素を書き込む
-	blendDesc.RenderTarget[0].RenderTargetWriteMask =
-		D3D12_COLOR_WRITE_ENABLE_ALL;
-
-
-	// 裏面（時計回り）を表示しない
-	rasterizerDesc.CullMode = D3D12_CULL_MODE_BACK;
-	// 三角形の中を塗りつぶす
-	rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
-
-	// Shaderをコンパイルする
-	vertexShaderBlob = CompileShader(L"Object3d.VS.hlsl",
-		L"vs_6_0", sDirectXCommon->GetDxcUtils(), sDirectXCommon->GetDxcCompiler(), sDirectXCommon->GetIncludeHandler());
-	assert(vertexShaderBlob != nullptr);
-
-	pixelShaderBlob = CompileShader(L"Object3d.PS.hlsl",
-		L"ps_6_0", sDirectXCommon->GetDxcUtils(), sDirectXCommon->GetDxcCompiler(), sDirectXCommon->GetIncludeHandler());
-	assert(pixelShaderBlob != nullptr);
-
-	graphicsPipelineStateDesc.pRootSignature = rootSignature; // RootSignature
-	graphicsPipelineStateDesc.InputLayout = inputLayoutDesc; //InputLayout
-	graphicsPipelineStateDesc.VS = { vertexShaderBlob->GetBufferPointer(),
-	vertexShaderBlob->GetBufferSize() };//vertexShader
-	graphicsPipelineStateDesc.PS = { pixelShaderBlob->GetBufferPointer(),
-	pixelShaderBlob->GetBufferSize() };// PixelShader
-	graphicsPipelineStateDesc.BlendState = blendDesc;//BlendState
-	graphicsPipelineStateDesc.RasterizerState = rasterizerDesc; //ReterizerState
-	// 書き込むRTVの情報
-	graphicsPipelineStateDesc.NumRenderTargets = 1;
-	graphicsPipelineStateDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
-	//利用するトポロジ（形状）のタイプ。三角形
-	graphicsPipelineStateDesc.PrimitiveTopologyType =
-		D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
-	// どのように画面に色を打ち込むかの設定（気にしなくてよい）
-	graphicsPipelineStateDesc.SampleDesc.Count = 1;
-	graphicsPipelineStateDesc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK;
-
-	// DeptjStencilの設定
-	graphicsPipelineStateDesc.DepthStencilState = sDirectXCommon->GetDepthStencilDesc();
-	graphicsPipelineStateDesc.DSVFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
-
-
-
-	//実際に生成
-	graphicsPipelineState = nullptr;
-	hr_ = sDirectXCommon->GetDevice()->CreateGraphicsPipelineState(&graphicsPipelineStateDesc,
-		IID_PPV_ARGS(&graphicsPipelineState));
-	assert(SUCCEEDED(hr_));
-
-
-	
 	// Sprite用の頂点リソースを作る
 	vertexResourceSprite_ = CreateBufferResourceA(sDirectXCommon->GetDevice(), sizeof(VertexData) * 6);
 
@@ -156,16 +44,8 @@ void Sprite::Initialize() {
 	vertexDataSprite_[5].position = {640.0f,360.0f,0.0f,1.0f};
 	vertexDataSprite_[5].texcorrd = {1.0f,1.0f};
 
-	// 実際に頂点リソースを作る
-	materialResource = CreateBufferResourceA(sDirectXCommon->GetDevice(), sizeof(Vector4));
-
-	materialBufferView = CreateBufferView();
-	// 頂点リソースにデータを書き込む
-	materialData = nullptr;
-	// 書き込むためのアドレスを取得
-	materialResource->Map(0, nullptr, reinterpret_cast<void**>(&materialData));
-	// 色のデータを変数から読み込み
-	*materialData = Vector4(1.0f,1.0f,1.0f,0.0f);
+	
+	
 
 	// Sprite用のTransformationMatrix用のリソースを作る。Matrix4x4 1つ分のサイズを用意する
 	transformationMatrixResouceSprite = CreateBufferResourceA(sDirectXCommon->GetDevice(),sizeof(Matrix4x4));
@@ -177,20 +57,7 @@ void Sprite::Initialize() {
 	
 
 
-	//クライアント領域のサイズと一緒にして画面全体に表示
-	viewport.Width = (float)sWinApp->GetKClientWidth();
-	viewport.Height = (float)sWinApp->GetKClientHeight();
-	viewport.TopLeftX = 1;
-	viewport.TopLeftY = 1;
-	viewport.MinDepth = 0.0f;
-	viewport.MaxDepth = 1.0f;
-
-
-	// 基本的にビューポートと同じ矩形が構成されるようにする
-	scissorRect.left = 0;
-	scissorRect.right = sWinApp->GetKClientWidth();
-	scissorRect.top = 0;
-	scissorRect.bottom = sWinApp->GetKClientHeight();
+	
 	// Transform変数の初期化
 	transform = { {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,0.0f} };
 };
@@ -204,19 +71,11 @@ Matrix4x4 worldViewProjectionMatrixSprite = Multiply(worldMatrixSprite, Multiply
 };
 
 void Sprite::Draw() {
-	// 色のデータを変数から読み込み
-	*materialData = {1.0f,1.0f,1.0f,0.0f};
-	sDirectXCommon->GetCommandList()->RSSetViewports(1, &viewport);  //viewportを設定
-	sDirectXCommon->GetCommandList()->RSSetScissorRects(1, &scissorRect);    //Scirssorを設定:
-	// RootSignatureを設定。PSOに設定しているけど別途設定が必要
-	sDirectXCommon->GetCommandList()->SetGraphicsRootSignature(rootSignature);
-	sDirectXCommon->GetCommandList()->SetPipelineState(graphicsPipelineState);    //PSOを設定m
-	// Spriteの描画。変更なものだけ変更する
+	
+	
+	
 	sDirectXCommon->GetCommandList()->IASetVertexBuffers(0, 1, &vertexBufferViewSprite_); // VBVを設定
-	//形状を設定。PSOに設定しているものとはまた別。同じものを設定すると考えておけば良い
-	sDirectXCommon->GetCommandList()->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-	// マテリアルCBufferの場所を設定
-	sDirectXCommon->GetCommandList()->SetGraphicsRootConstantBufferView(0, materialResource->GetGPUVirtualAddress());
+	
 	// TransformationmatrixCBufferの場所を設定
 	sDirectXCommon->GetCommandList()->SetGraphicsRootConstantBufferView(1, transformationMatrixResouceSprite->GetGPUVirtualAddress());
 
@@ -228,6 +87,8 @@ void Sprite::Draw() {
 void Sprite::Release() {
 
 	transformationMatrixResouceSprite->Release();
+	vertexResourceSprite_->Release();
+	
 };
 
 ID3D12Resource* Sprite::CreateBufferResourceA(ID3D12Device* device, size_t sizeInBytes) {
--- Src/Sprite.h
+++ Src/Sprite.h
@@ -56,61 +56,12 @@ private:
 	// データを書き込む
 	Matrix4x4* transformationMatrixDataSprite = nullptr;
 
-	// シリアライズしてバイナリにする
-	ID3DBlob* signatureBlob;
-	ID3DBlob* errorBlob;
-
-	// バイナリを元に生成
-	ID3D12RootSignature* rootSignature;
-	
-	// InputLayout
-	D3D12_INPUT_ELEMENT_DESC inputElementDescs[2] = {};
-	D3D12_INPUT_LAYOUT_DESC  inputLayoutDesc{};
-
 	Transform transformSprite_;
 	
 	D3D12_DESCRIPTOR_RANGE descriptorRange_[1] = {};
 
 	// RootParmeter作成。複数でっていできるので配列。今回は結果１つだけなので長さ1の配列
-	D3D12_ROOT_PARAMETER rootParamerters[3] = {};
-
-	D3D12_STATIC_SAMPLER_DESC staticSamplers[1] = {};
-
-	// RootSignature作成
-	D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature{};
-
-	// blendStateの設定
-	D3D12_BLEND_DESC blendDesc{};
-	// RasiterzerStateの設定
-	D3D12_RASTERIZER_DESC rasterizerDesc{};
-
-	IDxcBlob* vertexShaderBlob;
-	IDxcBlob* pixelShaderBlob;
-	D3D12_GRAPHICS_PIPELINE_STATE_DESC graphicsPipelineStateDesc{};
-	//実際に生成
-	ID3D12PipelineState* graphicsPipelineState;
-	HRESULT hr_;
-
+	D3D12_ROOT_PARAMETER rootParamerters[1] = {};
 	Transform transform;
-	/*色用*/
-//頂点リソースの設定
-// 実際に頂点リソースを作る
-	ID3D12Resource* materialResource;
-	// 頂点バッファビューを作成する
-	D3D12_VERTEX_BUFFER_VIEW materialBufferView{};
-	// 頂点リソースにデータを書き込む
-	Vector4* materialData;
-
-
-
-
-	//ビューポート
-	D3D12_VIEWPORT viewport{};
-	// シザー矩形
-	D3D12_RECT scissorRect{};
-
-
-
-
 };
 
