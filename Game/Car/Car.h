00cc7d67e82e2d88b915eda3c89a4d2c51fc86fe
MatsubaraKai <k022g0104@g.neec.ac.jp>
2024-01-18 10:30:37 +0900

    file error

---
+++ Game/Car/Car.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "WorldTransform.h"
+#include "Model.h"
+#include "Camera.h"
+#include "Input.h"
+#include "IScene.h"
+#include "TextureManager.h"
+#include "Audio.h"
+#include "Particle.h"
+#include "Sprite.h"
+class Car
+{
+public:
+	Car();
+	~Car();
+	void Init();
+	void Update();
+	void Draw(Camera* camera);
+	void Release();
+private:
+	WorldTransform worldTransform_;
+	Model* model_ = nullptr;
+	uint32_t texture_ = 1;
+	Input* input = nullptr;
+	Vector4 color = { 1.0f,1.0f,1.0f,1.0f };
+	Sprite* sprite_ = nullptr;
+	Particle* particle = nullptr;
+
+	float Speed = 0.0f;
+	float NormalSpeed = 0.05f;
+	float ShiftSpeed = 0.1f;
+};
+
--- Game/Scene/GameScene.cpp
+++ Game/Scene/GameScene.cpp
@@ -2,8 +2,11 @@
 
 void GameScene::Init()
 {
+	camera = new Camera;
+	camera->Initialize();
 	input = Input::GetInstance();
-
+	car_ = new Car();
+	car_->Init();
 }
 
 void GameScene::Update()
@@ -14,16 +17,17 @@ void GameScene::Update()
 		sceneNo = CLEAR;
 		sceneTime = 0;
 	}
-	
+	camera->Update();
 
+	car_->Update();
 }
 void GameScene::Draw()
 {
-	
+	car_->Draw(camera);
 }
 
 void GameScene::Release() {
-
+	delete car_;
 }
 // ゲームを終了
 int GameScene::GameClose()
