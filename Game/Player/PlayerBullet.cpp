6833e5dcd19d4a2e06615b30b61303cf8b626ece
OikawaYuma <k022g0016@g.neec.ac.jp>
2024-01-18 11:40:54 +0900

    途中

---
+++ Game/Player/PlayerBullet.cpp
@@ -0,0 +1,16 @@
+#include "PlayerBullet.h"
+
+
+PlayerBullet::PlayerBullet() {};
+PlayerBullet::~PlayerBullet() {};
+
+void PlayerBullet::Init() {
+};
+
+void PlayerBullet::Update() {
+
+};
+
+void PlayerBullet::Draw() {
+
+}
