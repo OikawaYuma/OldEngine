24c2a919960a05294b83997aa449a0ad37bdca62
MatsubaraKai <k022g0104@g.neec.ac.jp>
2024-02-01 11:15:48 +0900

    a

--- Game/Car/Car.cpp
+++ Game/Car/Car.cpp
@@ -111,9 +111,9 @@ void Car::Draw(Camera* camera) {
 	case NormalMode: {
 		model_->Draw(worldTransform_, texture_, camera, color);
 		if (input->PushKey(DIK_LSHIFT)) {
-			particle3->Draw({ worldTransform_.translation_.x - 3 * move.y,worldTransform_.translation_.y+0.3f,worldTransform_.translation_.z - 3 * move.x }, texture3_, camera, rearLeft);
-			particle4->Draw({ worldTransform_.translation_.x - 3 * move.y,worldTransform_.translation_.y+0.3f,worldTransform_.translation_.z - 3 * move.x }, texture3_, camera, rearRight);
-		
+			particle3->Draw({ worldTransform_.translation_.x - 3 * move.y,worldTransform_.translation_.y + 0.3f,worldTransform_.translation_.z - 3 * move.x }, texture3_, camera, rearLeft);
+			particle4->Draw({ worldTransform_.translation_.x - 3 * move.y,worldTransform_.translation_.y + 0.3f,worldTransform_.translation_.z - 3 * move.x }, texture3_, camera, rearRight);
+
 		}
 		break;
 	}
@@ -129,8 +129,8 @@ void Car::Draw(Camera* camera) {
 		float theta = (rotate_ / 2.0f) * (float)M_PI;
 		Vector2 move2 = { cosf(theta),sinf(theta) };
 		model_->Draw(driftWT, texture_, camera, color);
-		particle->Draw({ driftWT.translation_.x - 2 * move2.y,driftWT.translation_.y-0.2f,driftWT.translation_.z - 2 * move2.x }, texture2_, camera, rearLeft);
-		particle2->Draw({ driftWT.translation_.x - 2 * move2.y,driftWT.translation_.y-0.2f,driftWT.translation_.z - 2 * move2.x }, texture2_, camera, rearRight);
+		particle->Draw({ driftWT.translation_.x - 2 * move2.y,driftWT.translation_.y - 0.2f,driftWT.translation_.z - 2 * move2.x }, texture2_, camera, rearLeft);
+		particle2->Draw({ driftWT.translation_.x - 2 * move2.y,driftWT.translation_.y - 0.2f,driftWT.translation_.z - 2 * move2.x }, texture2_, camera, rearRight);
 		break;
 	}
 	}
@@ -167,91 +167,91 @@ void Car::Move()
 	}*/
 	//if (Input::GetInstance()->GetJoystickState(joyState)) {
 
-		if (worldTransform_.rotation_.y > 1.5f) {
-			worldTransform_.rotation_.y = 1.5f;
-			rotate_ = 0.96f;
-		}
-		if (worldTransform_.rotation_.y < -1.5f) {
-			worldTransform_.rotation_.y = -1.5f;
-			rotate_ = -0.96f;
-		}
-		switch (driveMode_) {
-		case NormalMode: {
-			// 回転
-			if (worldTransform_.rotation_.y >= -1.5f && worldTransform_.rotation_.y <= 1.5f) {
-				rotate_ += (float)joyState.Gamepad.sThumbLX / SHRT_MAX * 0.04f;
-
-			}
-			// 移動
-			if (moveFlag_) {
-				worldTransform_.translation_.x += Speed * move.y;
-				worldTransform_.translation_.z += Speed * move.x;
-
-			}
-			worldTransform_.rotation_.z = 0.0f; 
-			worldTransform_.rotation_.x = 0.0f;
-			break;
-		}
-		case DriftMode: {
-			// 回転
-			/*if (worldTransform_.rotation_.y >= -1.5f && worldTransform_.rotation_.y <= 1.5f) {
-				rotate_ += (float)joyState.Gamepad.sThumbLX / SHRT_MAX * 0.06f;
-
-			}*/
-
-
-			if (rotate_ >= 0.05f) {
-				short leftStickX = joyState.Gamepad.sThumbLX;
-				if (leftStickX > XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE) {
-					rotate_ += 0.01f;
-					
-				}
-				else if (leftStickX < -XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE) {
-					rotate_ -= 0.005f;
-					worldTransform_.translation_.x -=1.5f * move.y;
-					
-					//worldTransform_.translation_.z -= 1.5f * move.x;
-				}
-				
-				if (rotate_ < 0.05f) {
-					rotate_ = 0.05f;
-				}
-				worldTransform_.rotation_.z = -0.05f;
-				worldTransform_.rotation_.x = -0.05f;
-			}
-			else if (rotate_ <= -0.05f) {
-
-				short leftStickX = joyState.Gamepad.sThumbLX;
-				if (leftStickX < -XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE) {
-					rotate_ -= 0.01f;
-				}
-				else if (leftStickX > XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE) {
-					rotate_ += 0.005f;
-					worldTransform_.translation_.x -= 1.5f * move.y;
-					//worldTransform_.translation_.z -= 1.5f * move.x;
-				}
-				//rotate_ += (float)joyState.Gamepad.sThumbLX / SHRT_MAX * 0.06f;
-				if (rotate_ > -0.05f) {
-					rotate_ = -0.05f;
-				}
-				worldTransform_.rotation_.z = 0.05f;
-				worldTransform_.rotation_.x = -0.05f;
-			}
-			
-
-
-			// 移動
-			if (moveFlag_) {
-				worldTransform_.translation_.x += DriftSpeed * move.y;
-				worldTransform_.translation_.z += DriftSpeed * move.x;
-			}
-
-			break;
-		}
-		}
-		//rotate_ += 0.04f;
-
+	if (worldTransform_.rotation_.y > 1.5f) {
+		worldTransform_.rotation_.y = 1.5f;
+		rotate_ = 0.96f;
 	}
+	if (worldTransform_.rotation_.y < -1.5f) {
+		worldTransform_.rotation_.y = -1.5f;
+		rotate_ = -0.96f;
+	}
+	switch (driveMode_) {
+	case NormalMode: {
+		// 回転
+		if (worldTransform_.rotation_.y >= -1.5f && worldTransform_.rotation_.y <= 1.5f) {
+			rotate_ += (float)joyState.Gamepad.sThumbLX / SHRT_MAX * 0.04f;
+
+		}
+		// 移動
+		if (moveFlag_) {
+			worldTransform_.translation_.x += Speed * move.y;
+			worldTransform_.translation_.z += Speed * move.x;
+
+		}
+		worldTransform_.rotation_.z = 0.0f;
+		worldTransform_.rotation_.x = 0.0f;
+		break;
+	}
+	case DriftMode: {
+		// 回転
+		/*if (worldTransform_.rotation_.y >= -1.5f && worldTransform_.rotation_.y <= 1.5f) {
+			rotate_ += (float)joyState.Gamepad.sThumbLX / SHRT_MAX * 0.06f;
+
+		}*/
+
+
+		if (rotate_ >= 0.05f) {
+			short leftStickX = joyState.Gamepad.sThumbLX;
+			if (leftStickX > XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE) {
+				rotate_ += 0.01f;
+
+			}
+			else if (leftStickX < -XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE) {
+				rotate_ -= 0.005f;
+				worldTransform_.translation_.x -= 1.5f * move.y;
+
+				//worldTransform_.translation_.z -= 1.5f * move.x;
+			}
+
+			if (rotate_ < 0.05f) {
+				rotate_ = 0.05f;
+			}
+			worldTransform_.rotation_.z = -0.05f;
+			worldTransform_.rotation_.x = -0.05f;
+		}
+		else if (rotate_ <= -0.05f) {
+
+			short leftStickX = joyState.Gamepad.sThumbLX;
+			if (leftStickX < -XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE) {
+				rotate_ -= 0.01f;
+			}
+			else if (leftStickX > XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE) {
+				rotate_ += 0.005f;
+				worldTransform_.translation_.x -= 1.5f * move.y;
+				//worldTransform_.translation_.z -= 1.5f * move.x;
+			}
+			//rotate_ += (float)joyState.Gamepad.sThumbLX / SHRT_MAX * 0.06f;
+			if (rotate_ > -0.05f) {
+				rotate_ = -0.05f;
+			}
+			worldTransform_.rotation_.z = 0.05f;
+			worldTransform_.rotation_.x = -0.05f;
+		}
+
+
+
+		// 移動
+		if (moveFlag_) {
+			worldTransform_.translation_.x += DriftSpeed * move.y;
+			worldTransform_.translation_.z += DriftSpeed * move.x;
+		}
+
+		break;
+	}
+	}
+	//rotate_ += 0.04f;
+
+}
 //}
 
 void Car::Drift()
@@ -264,7 +264,7 @@ void Car::Drift()
 
 		driveMode_ = NormalMode;
 	}
-	else if (input->PushKey(DIK_S) || (joyState.Gamepad.wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER) &&!(rotate_<0.05f&& rotate_>-0.05)) {
+	else if (input->PushKey(DIK_S) || (joyState.Gamepad.wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER) && !(rotate_<0.05f && rotate_>-0.05)) {
 		driveMode_ = DriftMode;
 		/*if (rotate_ > 0) {
 			float theta = (rotate_ + 0.1f / 2.0f) * (float)M_PI;
