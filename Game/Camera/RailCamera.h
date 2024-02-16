af3b04e6c286898f72cf78d62bc3617545b7e123
OikawaYuma <k022g0016@g.neec.ac.jp>
2024-02-16 14:03:44 +0900

    jj

--- Game/Camera/RailCamera.h
+++ Game/Camera/RailCamera.h
@@ -1,5 +1,13 @@
 #pragma once
+#include "Camera.h"
+#include "WorldTransform.h"
 class RailCamera
 {
+public:
+	void Init();
+	void Update();
+private:
+	WorldTransform worldTransform_;
+	Camera* camera = nullptr;
 };
 
