df02d43c80ea3dbf2839ba5622a9781bf2041d4e
OikawaYuma <k022g0016@g.neec.ac.jp>
2024-06-17 10:50:55 +0900

    完成

--- project/Resources/shader/Random.PS.hlsl
+++ project/Resources/shader/Random.PS.hlsl
@@ -8,18 +8,22 @@ struct PixelShaderOutput {
 };
 
 
-float32_t rand2dTo1d(float32_t2 input)
+float32_t rand2dTo1d(float32_t2 input, float32_t2 dotDir = float32_t2(12.9898f,78.233f))
 {
-    float32_t2 smallValue = sin(3.0f);
-    float32_t random = dot(smallValue, input);
+    float32_t2 smallValue = sin(input);
+    float32_t random = dot(smallValue, dotDir);
     random = frac(sin(random) * 143758.5453f);
     return random;
 }
 
 float32_t rand(float32_t3 vec)
 {
-    float32_t random = dot(vec, float32_t3(12.9898, 78.233, 37.719));
-    random = frac(random * 143758.5453);
+     //make value smaller to avoid artefacts
+    float32_t3 smallValue = sin(vec);
+    //get scalar value from 3d vector
+    float32_t random = dot(smallValue, float32_t3(12.9898f, 78.233f, 37.719f));
+    //make value more random by making it bigger and then taking teh factional part
+    random = frac(sin(random) * 143758.5453f);
     return random;
 }
 
@@ -27,7 +31,7 @@ PixelShaderOutput main(VertexShaderOutput input)
 {
 	PixelShaderOutput output;
 	// 乱数生成。引数にtexcoordを和刺している
-    float32_t random = rand(float32_t3(input.texcoord.x, input.texcoord.y,1.0f));
+    float32_t random = rand2dTo1d(float32_t2(input.texcoord.x, input.texcoord.y));
 	// 色にする
         output.color = float32_t4(random, random, random, 1.0f);
         return output;
